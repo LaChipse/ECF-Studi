0ee6d34f9f9f36749925667edde5871b5764555d
VCombettes <vivi_c@hotmail.fr>
2022-04-18 16:25:53 +0200

    suppression dossier app/

--- app/asset/js/addSection.js
+++
@@ -1,7 +0,0 @@
-var count = 0
-
-/* Ajoute un input pour ajouter une section supllementaire */
-$(".addSection").click(function() {
-    count++
-    $(".sectionInput").append('<label for="titre' + count + '" class="form-label">Titre de la section</label><input type="text" name="titre' + count + '" class="form-control mb-4" id="titre' + count + '" required></input>');
-});
--- app/asset/js/fade.js
+++
@@ -1,7 +0,0 @@
-/* Faire apparaitre les btn des formations en catalogue */
-$(document).ready(function() {
-    $("button").click(function() {
-        $(this).next('.btnManage').fadeIn();
-        $(this).next('.btnManage').css("display", "flex");
-    });
-});
